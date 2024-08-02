ec3ae9462c72085e06fddc2c5f14265a4f6b26c2
chenhe <chnhechen@gmail.com>
2024-08-02 17:31:01 +0800

    i am doing my feature

--- app/src/main/java/he/chen/coroutinedemo/MainActivity.kt
+++ app/src/main/java/he/chen/coroutinedemo/MainActivity.kt
@@ -27,6 +27,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // my own feature
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
